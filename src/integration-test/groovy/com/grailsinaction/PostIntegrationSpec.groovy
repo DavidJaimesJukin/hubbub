058070c8f0a8bbbce2b158a42a7a11a48f6b0a00
David Jaimes <david.jaimes@jukinmedia.com>
2017-04-05 08:18:34 -0700

    Added another integration tests that verifies posts can be listed

--- src/integration-test/groovy/com/grailsinaction/PostIntegrationSpec.groovy
+++ src/integration-test/groovy/com/grailsinaction/PostIntegrationSpec.groovy
@@ -21,4 +21,23 @@ class PostIntegrationSpec extends Specification {
         then: "The user has a list of posts attached"
         3 == User.get(user.id).posts.size()
     }
+
+    void "Ensure posts linked to a user can be retrieved"(){
+        given: "A user with several posts"
+        def user = new User(loginId: 'joe', password: 'secret')
+        user.save(failOnError: true)
+        user.addToPosts(new Post(content: "First"))
+        user.addToPosts(new Post(content: "second"))
+        user.addToPosts(new Post(content: "third"))
+
+
+        when:"The user is retrieved by their id"
+        def foundUser= User.get(user.id)
+        def sortedPostContent= foundUser.posts.collect {
+            it.content
+        }.sort()
+
+        then: "The posts appear on the retrieved user"
+        sortedPostContent == ['First','second', 'third']
+    }
 }
