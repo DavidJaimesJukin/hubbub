5c3d22287e4c89386353532ebf9eadab85090ad3
David Jaimes <david.jaimes@jukinmedia.com>
2017-04-05 05:04:46 -0700

    First integraton test runs correctly and passes

--- src/integration-test/groovy/com/grailsinaction/UserIntegrationSpec.groovy
+++ src/integration-test/groovy/com/grailsinaction/UserIntegrationSpec.groovy
@@ -8,15 +8,14 @@ import spock.lang.*
 @Integration
 @Rollback
 class UserIntegrationSpec extends Specification {
-
-    def setup() {
-    }
-
-    def cleanup() {
-    }
-
-    void "test something"() {
-        expect:"fix me"
-            true == false
+    void "Saving you first user to the database"(){
+        given:"a brand new user"
+        def joe = new User(loginId: 'Joe', password: 'secret',homepage: 'http://www.grailsinaction.com')
+        when:"The user is saved"
+        joe.save()
+        then:"is saed successfully and can be found in the database"
+        joe.errors.errorCount == 0
+        joe.id != null
+        User.get(joe.id).loginId == joe.loginId
     }
 }
