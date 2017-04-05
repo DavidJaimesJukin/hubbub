04fac614736bf3ebef26df653b69451998aabe3a
David Jaimes <david.jaimes@jukinmedia.com>
2017-04-05 05:42:31 -0700

    Finished integration tests for user as well as user validation added ApplicationUser class

--- src/integration-test/groovy/com/grailsinaction/UserIntegrationSpec.groovy
+++ src/integration-test/groovy/com/grailsinaction/UserIntegrationSpec.groovy
@@ -18,4 +18,61 @@ class UserIntegrationSpec extends Specification {
         joe.id != null
         User.get(joe.id).loginId == joe.loginId
     }
+
+    void "Updating a saved users changes its properties"(){
+        given: "An existing user"
+        def existingUser = new User(loginId: 'joe', password: 'secret', homepage: 'http://www.grailsinaction.com')
+        existingUser.save(failOnError:true)
+        when: "A property is changed"
+        def foundUser = User.get(existingUser.id)
+        foundUser.password = 'seasame'
+        foundUser.save(failOnError:true)
+        then:"The change is reflected in the database"
+        User.get(existingUser.id).password == 'seasame'
+    }
+
+    void "Deleting an existing user removes it from the database"(){
+        given: "An existing user"
+        def user = new User(loginId: 'joe', password: 'secret', homepage: 'http://www.grailsinaction.com')
+        user.save(failOnError: true)
+
+        when: "the user is deleted"
+        def foundUser = User.get(user.id)
+        foundUser.delete(flush:true)
+
+        then:"The user is removed from the database"
+        !User.exists(foundUser.id)
+    }
+
+    void "Saving a user with invalid properties causes an error"(){
+        given: "A user which fails several field validations"
+        def user = new User(loginId: 'joe', password: 'tiny', homepage: 'not-a-url')
+
+        when: "The user is validated"
+        user.validate()
+
+        then:
+        user.hasErrors()
+
+        "size.toosmall" == user.errors.getFieldError("password").code
+        "tiny" == user.errors.getFieldError("password").rejectedValue
+        "url.invalid" == user.errors.getFieldError("homepage").code
+        "not-a-url" == user.errors.getFieldError("homepage").rejectedValue
+    }
+
+    void "Recovering from a failed save by fixing invalid properties"(){
+        given: "A user that has invalid properties"
+        def chuck = new User(loginId: 'chuck', password: 'tiny', homepage: 'not-a-url')
+        assert chuck.save() == null
+        assert chuck.hasErrors()
+
+        when:"We fix the invalid properties"
+        chuck.password = 'fistfist'
+        chuck.homepage = 'http://www.chucknorrisfacts.com'
+        chuck.validate()
+
+        then:"Then the user saves and validates fine"
+        !chuck.hasErrors()
+        chuck.save()
+    }
 }
