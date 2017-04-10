b27c0e4c447880ef9e79a6652a617c631f58a9d4
David Jaimes <david.jaimes@jukinmedia.com>
2017-04-10 15:50:58 -0700

    Added test user to bootstrap

--- grails-app/init/hubbub/BootStrap.groovy
+++ grails-app/init/hubbub/BootStrap.groovy
@@ -1,9 +1,31 @@
 package hubbub
 
+import com.grailsinaction.Profile
+import com.grailsinaction.User
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
+        switch (Environment.current){
+            case Environment.DEVELOPMENT:
+                createAdminUserIfRequired()
+                break
+            case Environment.PRODUCTION:
+                println("No specials config is required")
+                break
+        }
     }
     def destroy = {
+    }
+
+    void createAdminUserIfRequired(){
+        if(User.count() == 0){
+            println("Fresh database creating new ADMIN user")
+            def profile = new Profile(email: "admin@yourhost.com")
+            def user = new User(loginId: 'Admin', password: 'secret', profile: profile).save()
+        } else{
+            println("Existing admin user, skipping creation")
+        }
     }
 }
