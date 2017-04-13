9d8a6264b86e4a1ca0fa75337995d78f4e9abd57
David Jaimes <david.jaimes@jukinmedia.com>
2017-04-13 14:19:27 -0700

    Added unit test for adding posts to timeline

--- src/test/groovy/com/grailsinaction/PostControllerSpec.groovy
+++ src/test/groovy/com/grailsinaction/PostControllerSpec.groovy
@@ -28,4 +28,24 @@ class PostControllerSpec extends Specification {
         model.user.loginId == 'chuck_norris'
         model.user.posts.size() == 2
     }
+
+    void "Adding a valid new post to the timeline"(){
+        given: "A user with posts in the db"
+        User chuck = new User(loginId: 'chuck_norris', password: 'secret')
+        chuck.save(failOnError:true)
+
+        and: "A loginId parameter"
+        params.id = chuck.loginId
+
+        and: "some content for the post"
+        params.content = "Chuck Norris can test and entire application in one unit test"
+
+        when: "addPost is invoked"
+        def model = controller.addPost()
+
+        then: "Our flash message and redirect confirms the success"
+        flash.message == 'Succesfully created post'
+        response.redirectedUrl == '/post/timeline/chuck_norris'
+        Post.countByUser(chuck) == 1
+    }
 }
