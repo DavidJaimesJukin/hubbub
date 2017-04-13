e3317f355a8273aa1e8b8fecd767ba274e14b6ba
David Jaimes <david.jaimes@jukinmedia.com>
2017-04-13 08:27:19 -0700

    Added new post function to the timeline. contains error catching as well.

--- grails-app/controllers/com/grailsinaction/PostController.groovy
+++ grails-app/controllers/com/grailsinaction/PostController.groovy
@@ -14,4 +14,23 @@ class PostController {
         }
     }
 
+    def addPost(){
+        def user = User.findByLoginId(params.id)
+        if(user){
+            def post = new Post(params)
+            user.addToPosts(post)
+            if(user.save()){
+                user.save(flush: true)
+                println(user.posts)
+                flash.message = "Succesfully created post"
+            } else {
+                flash.message = "Invalid or empty post"
+            }
+        } else {
+            flash.message = "Invalid user ID"
+        }
+
+        redirect(action: 'timeline', id: params.id)
+    }
+
 }
